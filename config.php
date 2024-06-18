cfba7592b963709790c520266bb81bb99595eca5
amila-abeysekara <abeysekaraamila214@gmail.com>
2024-06-18 11:15:23 +0530

    DigitalStrategies

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Configuration of the site */
+// define('DATABASE_SERVER',   'localhost');
+// define('DATABASE_USERNAME', 'root');
+// define('DATABASE_PASSWORD', '');
+// define('DATABASE_NAME',     'AI');
+// define('SITE_URL',          'http://localhost/product/');
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'awsstart_digitalstrategistsco_gen_ai');
+define('DATABASE_PASSWORD', 'TxYrjrfjN64M98j2T9c4');
+define('DATABASE_NAME',     'awsstart_digitalstrategistsco_gen_ai');
+define('SITE_URL',          'https://digitalstrategists.co/');
